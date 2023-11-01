e077d961fb3846626926ef70e50b58421cbce14b
bportelalp <bportelalopez@outlook.es>
2023-11-01 17:30:05 +0100

    Tests de DbContext

---
+++ test/TestxUnitTraining.Module5.Advanced.Tests/ServerTests/ChatDbContextTests.cs
@@ -0,0 +1,48 @@
+﻿using Chat.Server.Library.Data;
+using FluentAssertions;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestxUnitTraining.Module5.Advanced.Tests.ServerTests.Helpers;
+
+namespace TestxUnitTraining.Module5.Advanced.Tests.ServerTests
+{
+    [Trait("Module", "5")]
+    public class ChatDbContextTests : IDisposable
+    {
+        private readonly ChatDbContext _dbContext;
+        private readonly SqliteConnection _connection;
+        public ChatDbContextTests()
+        {
+            _connection = new SqliteConnection("DataSource=:memory:");
+            _connection.Open();
+
+
+            var options = new DbContextOptionsBuilder<ChatDbContext>()
+                .UseSqlite(_connection)
+                .Options;
+
+            _dbContext = new ChatDbContext(options);
+            _dbContext.Database.EnsureCreated();
+            InitializationDatabase.InitializeContext(_dbContext);
+        }
+
+        [Fact]
+        public void User1_ShouldHaveTwoMessages()
+        {
+            var user1Messages = _dbContext.ChatMessages.Where(m => m.Author == "User1");
+
+            user1Messages.Should().HaveCount(2);
+        }
+
+        public void Dispose()
+        {
+            _dbContext?.Dispose();
+            _connection?.Dispose();
+        }
+    }
+}
---
+++ test/TestxUnitTraining.Module5.Advanced.Tests/ServerTests/Helpers/InitializationDatabase.cs
@@ -0,0 +1,34 @@
+﻿using Chat.Common.Entities;
+using Chat.Server.Library.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestxUnitTraining.Module5.Advanced.Tests.ServerTests.Helpers
+{
+    internal static class InitializationDatabase
+    {
+        private static readonly List<ChatMessage> _messages = new List<ChatMessage>()
+        {
+            new ChatMessage(){ IdChatMessage = 1, Author = "User1", Date = DateTime.Now, Message = "Message 1of User1" },
+            new ChatMessage(){ IdChatMessage = 2, Author = "User2", Date = DateTime.Now, Message = "Message 1 of User2" },
+            new ChatMessage(){ IdChatMessage = 3, Author = "User1", Date = DateTime.Now, Message = "Message 2 of User1" },
+            new ChatMessage(){ IdChatMessage = 4, Author = "User2", Date = DateTime.Now, Message = "Message 2 of User2" },
+        };
+
+        private static readonly List<ChatUser> _users = new List<ChatUser>()
+        {
+            new ChatUser(){IdUser = 1, Name = "User1", Password="PassUser1"},
+            new ChatUser(){IdUser = 2, Name = "User2", Password="PassUser2"}
+        };
+
+        public static void InitializeContext(ChatDbContext context)
+        {
+            context.ChatUsers.AddRange(_users);
+            context.ChatMessages.AddRange(_messages);
+            context.SaveChanges();
+        }
+    }
+}
